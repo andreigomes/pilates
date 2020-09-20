79418fa307dbf8449f8febde9a9d6e05a5b41e46
Andrei Gomes <andrei_sps@hotmail.com>
2020-09-20 20:22:29 -0300

    Criando operacao de instanciacao

--- src/main/java/com/pilates/PilatesApplication.java
+++ src/main/java/com/pilates/PilatesApplication.java
@@ -1,13 +1,33 @@
 package com.pilates;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PilatesApplication {
+import com.pilates.model.Categoria;
+import com.pilates.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PilatesApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PilatesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	Categoria categoria1 = new Categoria(null, "Informática");
+	Categoria categoria2 = new Categoria(null, "Escritório");
+	
+	categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 
 }
