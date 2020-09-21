40568b3ecde1a2c22fb987a809b8e35fb80132f3
Andrei Gomes <andrei_sps@hotmail.com>
2020-09-20 21:07:52 -0300

    Associacao manyToMany

--- src/main/java/com/pilates/PilatesApplication.java
+++ src/main/java/com/pilates/PilatesApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pilates.model.Categoria;
+import com.pilates.model.Produto;
 import com.pilates.repositories.CategoriaRepository;
+import com.pilates.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PilatesApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PilatesApplication.class, args);
@@ -26,7 +31,20 @@ public class PilatesApplication implements CommandLineRunner {
 	Categoria categoria1 = new Categoria(null, "Informática");
 	Categoria categoria2 = new Categoria(null, "Escritório");
 	
+	Produto produto1 = new Produto(null, "Computador", 2000.00);
+	Produto produto2 = new Produto(null, "Impressora", 800.00);
+	Produto produto3 = new Produto(null, "Mouse", 80.00);
+
+	//categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+	//categoria1.getProdutos().addAll(Arrays.asList(produto2));
+	
+	
+	produto1.getCategorias().addAll(Arrays.asList(categoria1));
+	produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+	produto3.getCategorias().addAll(Arrays.asList(categoria1));	
+	
 	categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+	produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
 	}
 
